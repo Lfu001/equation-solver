199812da80810b0d2d43758b75b87fdc794957e4
Lfu001 <goodspeed913@gmail.com>
2022-03-26 23:12:11 +0900

    Add new solver (golden) to EquationSolver.

--- solver.py
+++ solver.py
@@ -16,7 +16,7 @@ class EquationSolver:
         """
         If possible, set `interval` to `(a, b)` satisfying f(a)f(b) < 0.
         """
-        all_solvers = {"bisect", "secant"}
+        all_solvers = {"bisect", "secant", "golden"}
         if solver not in all_solvers:
             raise ValueError("Equation Solver supports only solvers in {}, got {}.".format(all_solvers, solver))
         self.__solver = solver
@@ -42,7 +42,7 @@ class EquationSolver:
             )
         self.__interval_max, self.__interval_min = sorted(interval)
 
-        if solver == "bisect" and not isinstance(precision, numbers.Number) or precision <= 0:
+        if solver in {"bisect", "golden"} and not isinstance(precision, numbers.Number) or precision <= 0:
             raise ValueError("precision parameter expects unsigned number, got {}.".format(precision))
         self.__precision = precision
 
@@ -74,6 +74,8 @@ class EquationSolver:
             return self.__binary_search()
         elif self.__solver == "secant":
             return self.__secant()
+        elif self.__solver == "golden":
+            return self.__golden()
 
     def __setInterval(self) -> None:
         interval = [self.__interval_min, self.__interval_max]
@@ -104,3 +106,26 @@ class EquationSolver:
                 break
             previous, current = current, current - self.__func(current) * (current - previous) / (self.__func(current) - self.__func(previous))
         return current
+
+    def __golden(self):
+        gamma = 0.3819660112501051
+        f = self.__func
+        a = self.__interval_min
+        b = self.__interval_max
+        a_prev = b_prev = None
+        while (b - a) > self.__precision:
+            if a == b or (a == a_prev and b == b_prev):
+                break
+            a_prev = a
+            b_prev = b
+            x1 = a + gamma * (b - a)
+            x2 = a + (1 - gamma) * (b - a)
+            if np.sign(f(a)) == np.sign(f(x1)):
+                a = x1
+                x1 = x2
+                x2 = a + (1 - gamma) * (b - a)
+            else:
+                b = x2
+                x2 = x1
+                x1 = a + gamma * (b - a)
+        return x1
